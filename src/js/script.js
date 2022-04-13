984959d1ad0a740bc6ddf309bff8bb2d709fe5a8
Emiliya <81705094+ccryptocoder@users.noreply.github.com>
2022-04-13 13:35:08 +0600

    Animate buttons and add the second tab

---
+++ src/js/script.js
@@ -0,0 +1,38 @@
+window.addEventListener('DOMContentLoaded', () => {
+    let siMore = Array.from(document.querySelectorAll('.si-more'));
+    let siBtnMore = Array.from(document.querySelectorAll('.si-btn-more'));
+
+    //показать текст при нажатии на кнопку Подробнее
+    siBtnMore.forEach((btn, index) => {
+        btn.addEventListener('click', (e) => {
+            e.preventDefault();
+            if ( !(siMore[index].classList.contains('si-more_active')) ) {
+                siMore[index].classList.add('si-more_active');
+                siMore[index].style.height = siMore[index].scrollHeight + 'px'; //присвоить тексту высоту его контента (вместо height: auto, иначе trsansition не будет работать) 
+                btn.innerText = 'Скрыть';
+            } else if ( siMore[index].classList.contains('si-more_active') ) {
+                siMore[index].style.height = '0px';
+                siMore[index].classList.remove('si-more_active');
+                btn.innerText = 'Подробнее';
+            }
+        });
+    });
+
+    //переключение между вкладками Услуги
+    let serviceTabBtns = Array.from(document.querySelectorAll('.service__group-btn'));
+    let serviceBtnSlider = document.querySelector('.service__group-slider');
+    let serviceTabs = Array.from(document.querySelectorAll('.service__tab'));
+
+    serviceTabBtns.forEach((btn, index) => {
+        btn.addEventListener('click', (e) => {
+            e.preventDefault();
+            serviceBtnSlider.classList.toggle('service__group-slider_active');
+            serviceTabs.forEach(tab => {
+                if(tab.classList.contains('service__tab_active')) {
+                    tab.classList.remove('service__tab_active')
+                }
+            });
+            serviceTabs[index].classList.add('service__tab_active');
+        });
+    });
+});
